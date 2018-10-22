006a6a774bd0c76a3f4b53ece26835abcbc11977
FifthRooter <toms.bernhards@gmail.com>
2018-10-22 12:15:28 +0000

    updated motor test file

--- js/motor-test.js
+++ js/motor-test.js
@@ -1,6 +1,7 @@
 const nanotimer = require('nanotimer')
 const gpio = require('pigpio').Gpio
 
+//const gpio = pigpio.initialize()
 const timer = new nanotimer()
 
 const direction_left = new gpio(16, {mode: gpio.OUTPUT})
@@ -9,8 +10,8 @@ const direction_right = new gpio(12, {mode: gpio.OUTPUT})
 const motor_pwm_left = new gpio(20, {mode: gpio.OUTPUT})
 const motor_pwm_right = new gpio(21, {mode: gpio.OUTPUT})
 
-let leftMotorSpeed = 75
-let rightMotorSpeed = 30
+let leftMotorSpeed = 100
+let rightMotorSpeed = 100
 
 timer.setInterval(() => {
   direction_left.digitalWrite(0)
@@ -19,8 +20,16 @@ timer.setInterval(() => {
   motor_pwm_left.pwmWrite(leftMotorSpeed)
   motor_pwm_right.pwmWrite(rightMotorSpeed)
 
-}, '5m', (err) => {
+},[""], '5m', (err) => {
   if (err) {
     console.log('Something went wrong with timer initialization :(')
   }
 })
+
+process.on('SIGINT', () => {
+ timer.clearInterval()
+//gpio.terminate()
+motor_pwm_left.pwmWrite(0)
+motor_pwm_right.pwmWrite(0)
+process.exit()
+});
